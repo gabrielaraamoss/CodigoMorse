7973944134171637035e6191a1a4b8e7000255f1
Gabriela Ramos <grramos@espol.edu.ec>
2020-11-23 21:06:51 -0500

    .

---
+++ ProyectoPP/src/main/java/ec/edu/espol/controller/UsuarioFXMLController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espol.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import javafx.scene.input.MouseEvent;
+/**
+ * FXML Controller class
+ *
+ * @author gabrielaramos
+ */
+public class UsuarioFXMLController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+    @FXML
+    private void ingresarP(MouseEvent event) {
+    }
+
+    @FXML
+    private void ingresarM(MouseEvent event) {
+    }
+
+}
--- ProyectoPP/src/main/java/module-info.java
+++ ProyectoPP/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module ec.edu.espol.gui {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens ec.edu.espol.gui to javafx.fxml;
     exports ec.edu.espol.gui;
